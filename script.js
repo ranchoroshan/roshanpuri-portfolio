7ecfb6f8c3dafa927ed187708c3c85c995dd14a6
ranchoroshan <itroshan06@gmail.com>
2025-04-16 09:48:45 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-
-const menuToggle = document.getElementById("menu-toggle");
-const navLinks = document.getElementById("nav-links");
-
-menuToggle.addEventListener("click", () => {
-  navLinks.classList.toggle("active");
-});
+document.getElementById('menu-toggle').addEventListener('click', () => document.getElementById('nav-links').classList.toggle('show'));
